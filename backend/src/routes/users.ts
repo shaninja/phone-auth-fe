39de24f8a4d8e364dceb8dab523be874f6838235
Shani Raby <shani.raby@gmail.com>
2023-11-16 16:25:21 +0000

    feat: fetch data from db if user already exists

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,9 +1,8 @@
-import { Request, Response, Router } from 'express';
-import admin from 'firebase-admin';
-const serviceAccount = require('../service-account-key.json');
-console.log("🚀 ~ file: users.ts:4 ~ serviceAccount:", serviceAccount)
+import { Request, Response, Router } from 'express'
+import admin from 'firebase-admin'
+const serviceAccount = require('../service-account-key.json')
 
-const router: Router = Router();
+const router: Router = Router()
 
 // TODO extract this to a place that can be used for the FE and BE alike
 const firebaseConfig = {
@@ -14,7 +13,7 @@ const firebaseConfig = {
   messagingSenderId: '501699185310',
   appId: '1:501699185310:web:0b85add1f990d32217898f',
   measurementId: 'G-QJWHNVEF4B',
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
   // TODO think about how to store the credentials. env var?
   // credential: admin.credential.cert({
   //   "project_id": process.env.FIREBASE_PROJECT_ID,
@@ -23,29 +22,40 @@ const firebaseConfig = {
   // }),
 }
 
-admin.initializeApp(firebaseConfig);
+admin.initializeApp(firebaseConfig)
 
-const db = admin.firestore();
+const db = admin.firestore()
 
 router.post('/updateUserDetails', async (req: Request, res: Response) => {
-  console.log('IN updateUserDetails');
   try {
     // TODO add server side validation for the fields, preferably use a validation library (for example Joi)
-    const {name, email, phone} = req.body
-    const userRef = db.collection('users').doc(phone);
-    const doc = await userRef.get();
-    
-    if (!doc.exists) {
-      await db.collection('users').doc(phone).set({name, email});
-    } else {
-      await db.collection('users').doc(phone).update({name, email});
-    }
-    
-    res.status(200).json({message: "Document updated successfully"});
-  } catch (error) {
-    console.log("🚀 ~ file: users.ts:30 ~ router.post ~ error:", error)
-    res.status(500).json({message: "Error updating document: ", error});
-  }
-});
+    const { name, email, phone } = req.body
+    const userRef = db.collection('users').doc(phone)
+    const doc = await userRef.get()
 
-export default router;
+    if (!doc.exists) {
+      await db.collection('users').doc(phone).set({ name, email })
+    } else {
+      await db.collection('users').doc(phone).update({ name, email })
+    }
+
+    res.status(200).json({ message: 'Document updated successfully' })
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating document: ', error })
+  }
+})
+
+router.get('/getUserDetails/:phone', async (req, res) => {
+  try {
+    const savedUser = await db.collection('users').doc(req.params.phone).get()
+    if (savedUser) {
+      res.status(200).json(savedUser.data())
+    } else {
+      res.status(404).json({ message: 'User not found' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching data: ', error })
+  }
+})
+
+export default router
--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,17 +1,56 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import firebase from 'firebase/compat/app'
 import 'firebase/auth'
 
 interface UserDetails {
   name: string
   email: string
+  phone: string
 }
 
 const ProfilePage: React.FC = () => {
   const [userDetails, setUserDetails] = useState<UserDetails>({
     name: '',
     email: '',
+    phone: '',
   })
+
+  const [firebaseUser, setFirebaseUser] = useState<firebase.User | null>(null)
+
+  useEffect(() => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        setFirebaseUser(user)
+      })
+    return () => unregisterAuthObserver()
+  }, [])
+
+  useEffect(() => {
+    const getUserDetails = async () => {
+      let phone: string | null = ''
+      try {
+        if (firebaseUser) {
+          phone = firebaseUser.phoneNumber
+
+          // TODO use env var for URL base
+          const response = await fetch(
+            `http://localhost:3000/users/getUserDetails/${phone}`,
+          )
+          const data = await response.json()
+          if (response.ok || response.status === 404) {
+            const savedUserDetails = { ...data, phone }
+            setUserDetails(savedUserDetails)
+          } else {
+            throw new Error('User not found')
+          }
+        }
+      } catch (error) {
+        console.error('Error:', error)
+      }
+    }
+    getUserDetails()
+  }, [firebaseUser])
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUserDetails({
@@ -24,30 +63,26 @@ const ProfilePage: React.FC = () => {
     // TODO for production we should use better validation
     // but for our purposes, html form validation is enough
     event.preventDefault()
-    
-    try {
-      const firebaseUser = firebase.auth().currentUser
-      let phone;
-      if (firebaseUser) {
-        phone = firebaseUser.phoneNumber
-      }
-      const userDetailsWithPhone = {
-        ...userDetails,
-        phone,
-      };
 
-      const response = await fetch('http://localhost:3000/users/updateUserDetails', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
+    try {
+      // TODO use env var for URL base
+      const response = await fetch(
+        'http://localhost:3000/users/updateUserDetails',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userDetails),
         },
-        body: JSON.stringify(userDetailsWithPhone)
-      });
+      )
       if (response.ok) {
-        console.log('Response:', await response.text());
+        // TODO add message to the user
+        console.log('Response:', await response.text())
       }
     } catch (e) {
-      console.error('Error:', e);
+      // TODO add message to the user
+      console.error('Error:', e)
     }
   }
 
