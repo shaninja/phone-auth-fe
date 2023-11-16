effb860adc6e694e066f29ea4dfbf3f6377fcb80
Shani Raby <shani.raby@gmail.com>
2023-11-16 16:16:34 +0000

    format: formatting

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import usersRoutes from './routes/users';
-import cors from 'cors';
+import express from 'express'
+import usersRoutes from './routes/users'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use('/users', usersRoutes);
+app.use(cors())
+app.use(express.json())
+app.use('/users', usersRoutes)
 
 app.listen(3000, () => {
-  console.log('Express app is listening on port 3000');
-});
+  console.log('Express app is listening on port 3000')
+})
