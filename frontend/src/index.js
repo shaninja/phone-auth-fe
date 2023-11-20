27694f09d7018d03acda90e8742759bfaab574f0
Shani Raby <shani.raby@gmail.com>
2023-11-20 08:51:13 +0000

    chore: comment cleanup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
 import firebase from 'firebase/compat/app'
 import { getAnalytics } from 'firebase/analytics'
 import 'firebase/compat/auth'
 import * as firebaseui from 'firebaseui'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyBOYVQZkD49hYqnLEYZxGJM7QI1V0-s4rk',
   authDomain: 'phone-auth-12.firebaseapp.com',
@@ -18,7 +13,6 @@ const firebaseConfig = {
   measurementId: 'G-QJWHNVEF4B',
 }
 
-// Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
 
